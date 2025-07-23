11e3643c3ffac1c64873af42634c3fc19bd3dd92
oreoluwaaym <oreoluwaaym@gmail.com>
2025-07-23 13:13:52 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "EOA BOT",
   botOwner: 'Oreoluwa', // Your name
-  ownerNumber: '7086278147', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '07086278147', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
